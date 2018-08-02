861ff5f17c9d702c2e6e112fb3214e56bb270d64
KiK0S <kostya.amelichev@gmail.com>
2018-08-02 14:33:14 +0300

    looks like my fft is working

--- auth/converter.py
+++ auth/converter.py
@@ -4,11 +4,17 @@ import soundfile as sf
 import sounddevice as sd
 import math
 import cmath
+import sys
+import os
 
 fs = 44100
 data = []
 segments = []
+real_part = []
+imag_part = []
 final = []
+print('whats your name');
+NAME = input();
 
 def read():
 	# print('rec')
@@ -17,57 +23,82 @@ def read():
 	# print('play')
 	# sd.play(n_data, fs)
 	# sd.wait()
-	n_data = sf.read('kostya_voice.wav')
+	# # print(n_data.shape)
+	n_data = sf.read(NAME + '_voice.wav') # if use this, use n_data[0]
+	n_data = n_data[0]
+	# n_data = []
+	# for i in range(0, fs * 2):
+	# 	n_data.append(math.sin(10 * 2 * math.pi * i / fs) + 0.5 * math.sin(5 * 2 * math.pi * i / fs))
+	# plt.plot(n_data)
+	# plt.show()
+	# sf.write(NAME + '_voice.wav', n_data, samplerate=fs);
 	global data 
-	data = [0 for i in range(0, len(n_data[0]))]
-	# print(n_data)
+	data = [0 for i in range(0, len(n_data))]
+	# print(data)
 	for i in range(0, len(data)):
-		data[i] = n_data[0][i]
+		data[i] = n_data[i]
 
-def normalize():
+def normalize(val):
 	mx = -100
-	for it in data:
-		mx = max(mx, it)
-	for it in data:
-		it /= mx
+	for it in val:
+		mx = max(mx, abs(it))
+	for i in range(0, len(val)):
+		val[i] /= mx
 
 def hemming(id):
+	pass
 	for i in range(0, LEN):
 		segments[id][i] *= 0.53836 - 0.46164 * math.cos(2 * math.pi * i / LEN)
 
 
-def fft(id, inv):
-	res = []
-	cntdots = 1000
-	for i in range(0, cntdots):
-		s = complex(0, 0)
-		for j in range(0, LEN):
-			s += segments[id][j] * cmath.exp(-2j * math.pi * i * j / LEN)
-		res.append(s.real)
-	return res
+def fft(A):
+	if len(A) == 1:
+		return A
+	n =	len(A)
+	a, b = [], []
+	for i in range(0, n):
+		if (i % 2 == 0):
+			a.append(A[i])
+		else:
+			b.append(A[i])
+	a = fft(a)
+	b = fft(b)
+	pw = complex(1)
+	w = complex(math.cos(2 * math.pi / n), math.sin(2 * math.pi / n))	
+	for i in range(0, n // 2):
+		A[i] = a[i] + pw * b[i]
+		A[i + n // 2] = a[i] - pw * b[i]
+		pw *= w
+	return A
 	
 
 def convert(id):
 	hemming(id)
-	segments[id] = fft(id, 0)
+	segments[id] = fft(segments[id])
 
 read()
-normalize()
-
-LEN = fs // 10
-print(LEN)
-print(len(data))
-for i in range(LEN, len(data), LEN // 2):
+normalize(data)
+LEN = fs // 5
+for i in range(0, len(data), LEN // 2):
 	if i + LEN >= len(data):
 		break
 	segments.append(data[i:i + LEN])
 	convert(len(segments) - 1)
 
-for i in range(0, len(segments[0])):
+for i in range(0, LEN):
 	sr = 0
+	si = 0
 	for j in range(0, len(segments)):
-		sr += segments[j][i]
+		sr += segments[j][i].real
+		si += segments[j][i].imag
 	sr /= len(segments)
-	final.append(sr)
-plt.plot(final)
+	si /= len(segments)
+	real_part.append(sr)
+	imag_part.append(si)
+normalize(real_part)
+normalize(imag_part)
+# plt.plot(data)
+plt.plot(real_part, 'b')
+# plt.plot(imag_part[0: 30], 'r')
+plt.savefig(NAME + '_fft.png')
 plt.show()
